814ea155552dd2c685a8b55a1a3e121d0b92bd57
Kacper Grzeszczyk <kacgrzes@gmail.com>
2017-11-04 23:51:16 +0100

    Fix generating index

--- src/generators/generateIndex.js
+++ src/generators/generateIndex.js
@@ -1,7 +1,7 @@
 const generateIndex = ({ name, jsExtension = 'js' }) =>
 `import ${name} from './${name}.${jsExtension}'
 
-export default Test
+export default ${name}
 `
 
 export default generateIndex
